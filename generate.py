8419de94b826372ae53d9d6523669185c6a62678
KyleBonner <kylebonner99@gmail.com>
2024-07-04 20:58:08 -0700

    feat: add excluded_character function

--- generate.py
+++ generate.py
@@ -10,8 +10,17 @@ def get_password_length():
         sys.exit(1)
     return length
 
+def get_excluded_characters():
+    try:
+        excluded_characters = sys.argv[2:]
+    except IndexError:
+        return ""
+    return excluded_characters
+
 length = int(get_password_length())
+excluded_characters = get_excluded_characters()
 password = []
+
 for i in range(length):
     password.append(string.printable[random.randint(0, 93)])
 
